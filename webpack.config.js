95b33a66cc49a310b49c404e3544c23ffa78a1e0
Deepesh Shrestha <deepeshshrestha@outlook.com>
2020-04-28 20:07:52 +0545

    Init Commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {test : /\.(js)$/, use:'babel-loader'},
+            {test : /\.css$/, use:['style-loader', 'css-loader']}
+        ]
+    },
+    mode:'dev',
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : 'src/index.html'
+        })
+    ]
+
+}
+
