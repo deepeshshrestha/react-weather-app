0394b9f6daf0500990e924c351b626bd3b4a074f
Deepesh Shrestha <deepeshshrestha@outlook.com>
2020-04-28 20:23:27 +0545

    Get current location

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,20 @@ const WeatherWrapper = styled.div`
   padding: 60;
 `;
 
+const TodayWeather = styled.div`
+  position: relative;
+  border: 1px solid black;
+  border-bottom: none;
+  height: 300;
+`;
+
+const FutureWeather = styled.div`
+  display: flex;
+  border: 1px solid black;
+  align-items: center;
+  padding: 10 0;
+`;
+
 class App extends React.Component {
   constructor() {
     super();
@@ -15,10 +29,28 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.getMyLocation();
+  }
+
+  getMyLocation() {
+    const location = window.navigator && window.navigator.geolocation;
+
+    if (location) {
+      location.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+      });
+    }
+  }
 
   render() {
-    return <WeatherWrapper></WeatherWrapper>;
+    return (
+      <WeatherWrapper>
+        <TodayWeather></TodayWeather>
+        <FutureWeather></FutureWeather>
+      </WeatherWrapper>
+    );
   }
 }
 
